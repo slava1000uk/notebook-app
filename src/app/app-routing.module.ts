16cfb67b0fd8e3251883637675e4535781431643
Viacheslav Tyshchuk <slava1000uk@gmail.com>
2023-07-29 00:46:25 +0200

    feat: add routing structure

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,25 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
 
-const routes: Routes = [];
+import { NotesComponent } from "./notes/notes.component";
+import { NoteDetailComponent } from "./notes/note-detail/note-detail.component";
+import { NoteStartComponent } from "./notes/note-start/note-start.component";
+import { NoteEditComponent } from "./notes/note-edit/note-edit.component";
+
+const appRoutes: Routes = [
+  { path: '', redirectTo: '/notes', pathMatch: 'full' },
+  {
+    path: 'notes', component: NotesComponent, children: [
+      { path: '', component: NoteStartComponent },
+      { path: 'new', component: NoteEditComponent },
+      { path: ':id', component: NoteDetailComponent },
+      { path: ':id/edit', component: NoteEditComponent }
+    ]
+  }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(appRoutes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
