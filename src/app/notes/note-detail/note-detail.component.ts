7ca98e38e61db98e8bdeae4ad236f97e49b2a7fc
Viacheslav Tyshchuk <slava1000uk@gmail.com>
2023-07-29 01:09:02 +0200

    feat: add note detail

--- src/app/notes/note-detail/note-detail.component.ts
+++ src/app/notes/note-detail/note-detail.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Note } from '../note.model';
+import { ActivatedRoute, Params } from '@angular/router';
+import { NoteService } from '../note.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-note-detail',
   templateUrl: './note-detail.component.html',
   styleUrls: ['./note-detail.component.css']
 })
-export class NoteDetailComponent {
+export class NoteDetailComponent implements OnInit, OnDestroy {
+  note: Note;
+  noteIdSubscription: Subscription;
+  noteId: number;
+
+  constructor(private route: ActivatedRoute,
+    private noteService: NoteService) { }
+
+  ngOnInit(): void {
+    this.noteId = +this.route.snapshot.params['id'];
+    this.note = this.noteService.getNote(this.noteId);
+
+    this.noteIdSubscription = this.route.params.subscribe(
+      (params: Params) => {
+        this.noteId = +params['id'];
+        this.note = this.noteService.getNote(this.noteId);
+      }
+    );
+
+  }
+
+  ngOnDestroy(): void {
+    this.noteIdSubscription.unsubscribe();
+  }
+
+  onDeleteNote() {
+    this.noteService.removeNote(this.noteId);
+  }
 
 }
