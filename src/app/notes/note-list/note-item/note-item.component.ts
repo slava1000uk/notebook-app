35cb3c42c446ff78cdea241812d0fbc1202a75aa
Viacheslav Tyshchuk <slava1000uk@gmail.com>
2023-07-29 00:52:45 +0200

    feat: add note-item component

--- src/app/notes/note-list/note-item/note-item.component.ts
+++ src/app/notes/note-list/note-item/note-item.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Note } from '../../note.model';
+import { NoteService } from '../../note.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-note-item',
   templateUrl: './note-item.component.html',
   styleUrls: ['./note-item.component.css']
 })
-export class NoteItemComponent {
+export class NoteItemComponent implements OnInit {
+  @Input() noteItem: Note;
+  noteId: number;
+
+  constructor(private noteService: NoteService,
+    private router: Router) { }
+
+
+
+  ngOnInit(): void {
+    this.noteId = this.noteService.getNoteId(this.noteItem);
+
+    this.noteService.notesUpdated
+      .subscribe(() => {
+        this.noteId = this.noteService.getNoteId(this.noteItem);
+
+        if (this.noteId === 0) {
+          const amountOfNotes = this.noteService.getNotes().length;
+
+          amountOfNotes === 0 ? this.router.navigate(['/notes', 'new']) : this.router.navigate(['/notes']);
+        }
+      });
+  }
 
 }
