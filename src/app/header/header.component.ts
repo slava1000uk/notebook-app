6659f67269fd2c1c8973c7dcb677130b99cc7854
Viacheslav Tyshchuk <slava1000uk@gmail.com>
2023-07-28 23:57:16 +0200

    feat: add header cmponent

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+
+  collapsed = true;
+
+  constructor() { }
+
+
+
+  ngOnInit(): void {
+  }
 
 }
