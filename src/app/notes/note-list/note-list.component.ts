a53406cc17c7268184fda333093058783a9f9ff1
Viacheslav Tyshchuk <slava1000uk@gmail.com>
2023-07-29 00:37:16 +0200

    feat: add note-list of note-items

--- src/app/notes/note-list/note-list.component.ts
+++ src/app/notes/note-list/note-list.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Note } from '../note.model';
+import { NoteService } from '../note.service';
 
 @Component({
   selector: 'app-note-list',
   templateUrl: './note-list.component.html',
   styleUrls: ['./note-list.component.css']
 })
-export class NoteListComponent {
+export class NoteListComponent implements OnInit {
+
+  notes: Note[];
+
+  constructor(private noteService: NoteService) { }
+
+
+  ngOnInit(): void {
+    this.notes = this.noteService.getNotes();
+
+    this.noteService.notesUpdated
+      .subscribe(() => {
+        this.notes = this.noteService.getNotes();
+      });
+  }
 
 }
